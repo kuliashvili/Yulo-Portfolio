71128b0636e2064659419120902e8ff617607cb0
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-06-12 13:40:22 +0400

    Main jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from "./components/header/Header"
+import Main from "./components/main/Main"
 
 function App() {
   return (
     <div className="App">
    
    <Header></Header>
+   < Main />
+   
     </div>
   );
 }
---
+++ src/components/main/Main.js
@@ -0,0 +1,22 @@
+import "./Main.css"
+
+function Main() {
+
+    return (
+        <div className="Main">
+            <div className="info">
+                <div className="info-inner">
+                    <div className="welcome"></div>
+                    <div className="welcome-header"></div>
+                    <div className="welcome-text"></div>
+                    <div className="button"></div>
+                </div>
+            </div>
+            <div className="profile-pic">
+                <img src="" alt="" className="profile-pic-item" />
+            </div>
+        </div>
+    )
+}
+
+export default Main
