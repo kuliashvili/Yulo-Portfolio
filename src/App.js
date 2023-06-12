6c073aa26856e3a6aac947e30952dff236f00095
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-06-12 11:58:08 +0400

    gh-pages today

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Wake Up Neo</h1>
+     <h1>Update Test From Terminal</h1>
+     <h1>briha</h1>
+    </div>
+  );
+}
+
+export default App;
